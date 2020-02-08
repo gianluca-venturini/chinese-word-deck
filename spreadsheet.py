6fc9ff5baf234b3daa6dc2400a14b2ffba7db60a
Gianluca Venturini <gianluca@stellarite.io>
2020-02-07 17:30:36 -0800

    Fix

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,9 +1,12 @@
 from __future__ import print_function
+
+import os
 import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
+
 from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
 
 class NoValuesFoundException(Exception):
     pass
